715f45a35d3b4f8c30c9265f256ebd5985f1c7cd
Zain Abbas <zainbbas3130@gmail.com>
2025-09-05 16:42:32 +0500

    fix port

--- app/main.py
+++ app/main.py
@@ -242,7 +242,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
-        port=5500,
+        port=4000,
         reload=settings.DEBUG,
         log_level="debug" if settings.DEBUG else "info"
     )
